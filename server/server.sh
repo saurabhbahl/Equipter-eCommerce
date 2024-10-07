1eb32b80a1e6f0117ee702435737eda36b9a611b
saurabhbahl <behl.saurabh@gmail.com>
2024-10-07 10:10:38 +0530

    fixed the docker configuration error

--- server/server.sh
+++ server/server.sh
@@ -1,35 +1,36 @@
 
 #!/bin/sh
+npm install
 
 make_db() {
-    sleep 20
-
   
+
     DB_NAME=${POSTGRES_DB}
 
- 
-    if PGPASSWORD=${POSTGRES_PASSWORD} psql -h postgress-database -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}'" | grep -q 1; then
-        echo "Database  already exists."
+    # Check if the database already exists
+    if PGPASSWORD=${POSTGRES_PASSWORD} psql -h postgres-database -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}'" | grep -q 1; then
+        echo "Database created!."
     else
         echo "Creating database '${DB_NAME}'..."
-        if ! PGPASSWORD=${POSTGRES_PASSWORD} psql -h postgress-database -U postgres -c "CREATE DATABASE ${DB_NAME};"; then
-            echo "Failed to create database. Exiting."
+      
+        if ! PGPASSWORD=${POSTGRES_PASSWORD} psql -h postgres-database -U postgres -c "CREATE DATABASE ${DB_NAME};"; then
+            echo "Failed to create database . Exiting."
         fi
     fi
 
-    sleep 10
+    sleep 5
 }
 
 make_db
 
 npm run db:generate
-sleep 12
+sleep 5
 
 npm run db:migrate
-sleep 15 
+sleep 5 
 
 npm run db:seed
-sleep 10
+sleep 5
 
 
 npm run start:dev
