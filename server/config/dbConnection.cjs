538e2c68fbc1495585f3d88b7f9b7942ebfb4531
saurabhbahl <behl.saurabh@gmail.com>
2024-10-08 10:06:42 +0530

    changed the orm and fixed shell script error

--- server/config/dbConnection.cjs
+++ server/config/dbConnection.cjs
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const { drizzle } = require("drizzle-orm/node-postgres");
 const connection = new Pool({
-  host: "db",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
